ad848da7df8a95247f8ffee33416114b4bc108f7
Salon6 <grg2051@outlook.com>
2025-11-21 12:59:53 -0600

    Version 0.15[PlayFab]

---
+++ Assets/Scripts/Manager/Managers.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+
+public class Managers : MonoBehaviour
+    //PlayFab Manager
+{
+    [Header("Login")]
+    [SerializeField] private TMP_InputField LoginEmail;
+    [SerializeField] private TMP_InputField loginPassword;
+
+    [Header("Create Account")]
+    [SerializeField] private TMP_InputField Ca_Username;
+    [SerializeField] private TMP_InputField Ca_Email;
+    [SerializeField] private TMP_InputField Ca_Password;
+    [SerializeField] private TMP_InputField Ca_ConfirmedPassword;
+    [SerializeField] private TMP_InputField Ca_AvatarUrl;
+
+    void Start()
+    {
+        if(PlayFabSettings.DeveloperSecretKey == null)
+        {
+            PlayFabSettings.DeveloperSecretKey = "Y5IW8KNPS8GYBWZSA1HDWR3TZRTK8A7PCIXF7HAA5GQQJHIU44";
+        }
+
+        if (PlayFabSettings.TitleId == null)
+        {
+            PlayFabSettings.TitleId = "1DA25";
+        }
+    }
+
+    public void CreateAccount()
+    {
+        RegisterPlayFabUserRequest request = new RegisterPlayFabUserRequest
+        {
+            Email = Ca_Email.text,
+            Username = Ca_Username.text.ToLower(),
+            DisplayName = Ca_Username.text,
+            Password = Ca_Password.text,
+
+        };
+
+
+        PlayFabClientAPI.RegisterPlayFabUser(request,OnCreateAccountSuccess,OnCreateAccountError);
+    }
+
+    public void OnCreateAccountSuccess(RegisterPlayFabUserResult result)
+    {
+        Debug.Log("");
+    }   
+    
+    public void OnCreateAccountError(PlayFabError error)
+    {
+        Debug.LogError(error);
+    }
+
+    
+}
